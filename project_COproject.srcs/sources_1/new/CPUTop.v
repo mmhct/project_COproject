6fb94b012fea62f2e9983e6c4cf34428e4a9ccc7
mmhct <12211026@mail.sustech.edu.cn>
2024-05-06 15:11:34 +0800

    1

--- project_COproject.srcs/sources_1/new/CPUTop.v
+++ project_COproject.srcs/sources_1/new/CPUTop.v
@@ -1,1 +1,12 @@
-parameter  = 22222;
+module CPUTop (
+    input fpga_rst, 
+    input fpga_clk, 
+    input[23:0] switch2N4, 
+    output[23:0] led2N4,
+
+    input start_pg,
+    input rx,
+    output tx,
+);
+    
+endmodule
